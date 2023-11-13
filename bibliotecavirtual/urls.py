c8bd51e933132108fd94e1023826ef0edd7c4302
Katherine Diaz <jkatherinediazt@gmail.com>
2023-11-12 21:37:15 -0400

    Adelanto

--- bibliotecavirtual/urls.py
+++ bibliotecavirtual/urls.py
@@ -2,7 +2,29 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('index',views.index,name='index'),
-    path('login',views.login,name='login'),
-    path('formulario',views.formulario,name='formulario')
+    path('index.html',views.index,name='index.html'),
+    path('login.html',views.login,name='login.html'),
+    path('formulario.html',views.formulario,name='formulario.html'),
+    path('admin.html',views.admin,name='admin.html'),
+    path('adminlist.html',views.adminlist,name='adminlist.html'),
+    path('adminsearch.html',views.adminsearch,name='adminsearch.html'),
+    path('book.html',views.book,name='book.html'),
+    path('bookconfig.html',views.adminsearch,name='bookconfig.html'),
+    path('bookinfo.html',views.bookinfo,name='bookinfo.html'),
+    path('catalog.html',views.catalog,name='catalog.html'),
+    path('category.html',views.category,name='category.html'),
+    path('categorylist.html',views.categorylist,name='categorylist.html'),
+    path('client.html',views.client,name='client.html'),
+    path('clientlist.html',views.clientlist,name='clientlist.html'),
+    path('clientsearch.html',views.clientsearch,name='clientsearch.html'),
+    path('company.html',views.company,name='company'),
+    path('companylist.html',views.companylist,name='companylist.html'),
+    path('home.html',views.home,name='home'),
+    path('myaccount.html',views.myaccount,name='myaccount.html'),
+    path('mydata.html',views.mydata,name='mydata.html'),
+    path('provider.html',views.provider,name='provider.html'),
+    path('providerlist.html',views.providerlist,name='providerlist.html'),
+    path('search.html',views.search,name='search.html'),
+
+
 ]
--- bibliotecavirtual/views.py
+++ bibliotecavirtual/views.py
@@ -12,3 +12,59 @@ def login(request):
 def formulario(request):
     return render(request,'paginas/formulario.html')
 
+def admin(request):
+    return render(request,'paginas/admin.html')
+
+def adminlist(request):
+    return render(request,'paginas/adminlist.html')
+
+def adminsearch(request):
+    return render(request,'paginas/adminsearch.html')
+
+def book(request):
+    return render(request,'paginas/book.html')
+
+def bookinfo(request):
+    return render(request,'paginas/bookinfo.html')
+
+def catalog(request):
+    return render(request,'paginas/catalog.html')    
+
+def category(request):
+    return render(request,'paginas/category.html')
+
+def categorylist(request):
+    return render(request,'paginas/categorylist.html')
+
+def client(request):
+    return render(request,'paginas/client.html')
+
+def clientlist(request):
+    return render(request,'paginas/clientlist.html')
+
+def clientsearch(request):
+    return render(request,'paginas/clientsearch.html')
+
+def company(request):
+    return render(request,'paginas/company.html')
+
+def companylist(request):
+    return render(request,'paginas/companylist.html')
+
+def home(request):
+    return render(request,'paginas/home.html')
+
+def myaccount(request):
+    return render(request,'paginas/myaccount.html')
+
+def mydata(request):
+    return render(request,'paginas/mydata.html')
+
+def provider(request):
+    return render(request,'paginas/provider.html')
+
+def providerlist(request):
+    return render(request,'paginas/providerlist.html')
+
+def search(request):
+    return render(request,'paginas/search.html')    
